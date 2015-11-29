555664644b9e09d8ebbe1a0a161f5e36976bd86a
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-29 17:35:38 +0100

    Can test fake ping delays

--- test/test.client.js
+++ test/test.client.js
@@ -1,7 +1,24 @@
 var socket = io('http://localhost:7777');
 
+function getQueryString () {
+  var qs = window.location.href.match(/[^\?]+\?(.+)/);
+  if (!qs) { return {}; }
+
+  var res = {};
+  qs = qs[1].split('&').forEach(function (e) {
+    res[e.split('=')[0]] = e.split('=')[1];
+  });
+
+  return res;
+}
+
+var qs = getQueryString();
+var delay = parseInt(qs.delay, 10) || 0;   // Ping delay for testing purposes
+
 socket.on('ping', function (data) {
-  console.log("Received ping");
-  socket.emit('pong', data);
+  var d = Date.now();
+  setTimeout(function () {
+    socket.emit('pong', data);
+  }, delay);
 });
 
