0e81e5eaa1481bc33240b69097b54b13c60cba43
Louis Chatriot <louis.chatriot@gmail.com>
2016-01-22 15:57:41 +0100

    Release script uses prod environment

--- client/release-android.sh
+++ client/release-android.sh
@@ -1,5 +1,17 @@
+# Switch configuration to production environment
+cp www/config.js www/config.js.bckp
+echo "var env='prod';" > www/env
+cat www/env www/config.js > www/config.js.new
+rm www/config.js
+rm www/env
+mv www/config.js.new www/config.js
+
 mkdir -p release
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ED_KEYSTORE_FILE platforms/android/build/outputs/apk/android-release-unsigned.apk $ED_KEYSTORE_ALIAS
 rm -f release/android.apk
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk release/android.apk
+
+# Back to dev environment
+rm www/config.js
+mv www/config.js.bckp www/config.js
---
+++ client/www/config.js
@@ -0,0 +1,8 @@
+var env='prod';
+var config = {};
+
+if (typeof env === 'undefined' || env === 'dev') {
+  config.server = 'http://localhost:7777';
+} else if (typeof env !== 'undefined' && env === 'prod') {
+  config.server = 'http://ed.lmt.io';
+}
