bc8822a9415110c4b2b3527884b0d4458424966b
Louis Chatriot <louis.chatriot@gmail.com>
2016-01-22 16:03:35 +0100

    Less error prone

--- client/release-android.sh
+++ client/release-android.sh
@@ -8,10 +8,12 @@ mv www/config.js.new www/config.js
 
 mkdir -p release
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ED_KEYSTORE_FILE platforms/android/build/outputs/apk/android-release-unsigned.apk $ED_KEYSTORE_ALIAS
-rm -f release/android.apk
-zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk release/android.apk
 
 # Back to dev environment
 rm www/config.js
 mv www/config.js.bckp www/config.js
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ED_KEYSTORE_FILE platforms/android/build/outputs/apk/android-release-unsigned.apk $ED_KEYSTORE_ALIAS
+rm -f release/android.apk
+zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk release/android.apk
+
